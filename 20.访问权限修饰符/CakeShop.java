399cd610fc3b13a5ec562b98451995d81b932a52
Shendi <1711680493@qq.com>
2020-08-16 11:17:49 +0800

    20

---
+++ 20.访问权限修饰符/CakeShop.java
@@ -0,0 +1,16 @@
+public class CakeShop {
+	
+	public void createQKLCake() {
+		createNaiyou();
+		System.out.println("创建了巧克力蛋糕");
+	}
+
+	public void createCMCake() {
+		createNaiyou();
+		System.out.println("创建了草莓蛋糕");
+	}
+
+	private void createNaiyou() {
+		System.out.println("获取了奶油");
+	}
+}
---
+++ 20.访问权限修饰符/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+		CakeShop cake = new CakeShop();
+		cake.createQKLCake();
+		cake.createCMCake();
+	}
+}
